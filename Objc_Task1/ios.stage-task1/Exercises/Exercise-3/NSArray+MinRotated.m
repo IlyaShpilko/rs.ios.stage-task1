f4ef0519deb207cfa65729034ba2317d4c77aa2f
IlyaShpilko <61794412+IlyaShpilko@users.noreply.github.com>
2021-05-08 16:27:19 +0400

    docs: third task completed

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -4,7 +4,49 @@
 
 - (NSNumber *)minRotated {
     
-    return nil;
-}
+    if ([self isEqual:@[]]) {
+        return nil;
+    }
+    
+    unsigned long left = 0;
+    unsigned long right = [self count] - 1;
+    
+    NSNumber *leftElement = [self objectAtIndex: left];
+    NSNumber *rightElement = [self objectAtIndex: right];
+    
+    if (rightElement.intValue >= leftElement.intValue) {
+        return leftElement;
+    }
+    
+    while (right >= left) {
+        unsigned long center = left + (right - left) / 2;
+        
+        NSNumber *centerNumber = [self objectAtIndex: center];
+        NSNumber *rightNumber = [self objectAtIndex: center + 1];
+        NSNumber *leftNumber = [NSNumber new];
+        NSNumber *zeroNumber = [self objectAtIndex: 0];
+        
+        if (center != 0) {
+            leftNumber = [self objectAtIndex: center - 1];
+        } else {
+            leftNumber = [self objectAtIndex: 0];
+        }
+        
+        if (centerNumber.intValue > rightNumber.intValue) {
+            return rightNumber;
+        }
+        
+        if (leftNumber.intValue > centerNumber.intValue) {
+            return centerNumber;
+        }
+        
+        if (centerNumber.intValue > zeroNumber.intValue) {
+            left = center + 1;
+        } else {
+            right = center - 1;
+        }
+    }
 
+        return nil;
+}
 @end
