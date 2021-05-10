2169bd6221e31042c55679c972d9fdb233d9f520
IlyaShpilko <61794412+IlyaShpilko@users.noreply.github.com>
2021-05-10 12:18:24 +0400

    docs: fifth task completed

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,28 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+    
+    NSMutableArray *mutab = [NSMutableArray new];
+    
+    int count = 0;
+    
+    NSMutableSet *set = [NSMutableSet new];
+    
+    for (long elementAtIndex = playerArray.count - 1; elementAtIndex >= 0; elementAtIndex--) {
+        
+        NSNumber *score = [playerArray objectAtIndex: elementAtIndex];
+        
+        while (count < rankedArray.count && score.intValue < [[rankedArray objectAtIndex:count] intValue] ) {
+            [set addObject:[rankedArray objectAtIndex:count]];
+            count++;
+        }
+        
+        id obj = @(set.count + 1);
+        
+        [mutab insertObject:obj atIndex:0];
+    }
+    
+    return mutab;
 }
 
 @end
